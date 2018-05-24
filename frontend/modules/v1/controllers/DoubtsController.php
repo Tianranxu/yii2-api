81394bab47f6bb3267fadc3a56e3a03d6ec86ece
xu <xu@gmail.com>
2018-05-24 19:28:37 +0800

    fix bug in doubts controller

--- frontend/modules/v1/controllers/DoubtsController.php
+++ frontend/modules/v1/controllers/DoubtsController.php
@@ -1,7 +1,9 @@
 <?php
 namespace frontend\modules\v1\controllers;
 
+use Yii;
 use yii\rest\ActiveController;
+use app\models\Doubts;
 
 class DoubtsController extends ActiveController {
     public $modelClass = 'app\models\Doubts';
@@ -11,7 +13,7 @@ class DoubtsController extends ActiveController {
         $doubts = Doubts::find()
         ->offset($request->post('limit')*($request->post('page')-1))
         ->limit($request->post('limit'))
-        ->orderBy(['create_time' => SORT_DESC])->all();
+        ->orderBy(['create_at' => SORT_DESC])->all();
         return $doubts;
     }
 
@@ -20,7 +22,7 @@ class DoubtsController extends ActiveController {
     }
 
     public function actionLike(){
-        
+
     }
 
     public function actionEncourage(){
