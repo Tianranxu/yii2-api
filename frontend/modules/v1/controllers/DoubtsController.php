211f0249fb9a3ebe1db54646aee704234c12355c
xu <xu@gmail.com>
2018-05-24 10:12:55 +0800

    add doubt controller and config

---
+++ frontend/modules/v1/controllers/DoubtsController.php
@@ -0,0 +1,12 @@
+<?php
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+class DoubtsController extends ActiveController {
+    public $modelClass = 'app\models\Doubts';
+
+    /*public function actionIndex(){
+        return ['msg' => 'hehe'];
+    }*/
+}
